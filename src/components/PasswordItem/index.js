ec892d53bc1e1f59d119f1453a9c548f9491f4f1
swathi kiran <swathikiran@example.com>
2023-11-05 16:17:14 +0000

    update files

--- src/components/PasswordItem/index.js
+++ src/components/PasswordItem/index.js
@@ -33,7 +33,7 @@ const PasswordItem = props => {
         type="button"
         className="delete-btn"
         data-testid="delete"
-        onClick={onClickDeleteBtn()}
+        onClick={onClickDeleteBtn}
       >
         <img
           src="https://assets.ccbp.in/frontend/react-js/password-manager-delete-img.png"
